aeb34e26af6810a74921a14087e96c4316052d2e
everyday-special <mlidell@gmail.com>
2022-02-14 16:11:35 -0800

    2/15/22

---
+++ c++/136_Single_Number/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int out = 0;
+        for (int i = 0; i < nums.size(); i++)
+            out = out ^ nums[i];
+        return out;
+    }
+};
